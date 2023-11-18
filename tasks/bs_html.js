9cf9ec38168d40f7cb24e36ad652d8e864c1a3f0
Нежданов Сергей <Tsebom@yandex.ru>
2023-11-18 23:05:04 +0400

    add comment and fix NOT FOUND FILE

--- tasks/bs_html.js
+++ tasks/bs_html.js
@@ -4,7 +4,7 @@ module.exports = function bs_html() {
 	browserSync.init({
 		server: {
 			baseDir: 'build/',
-			host: '192.168.1.77'
+			host: '192.168.1.77'//this is the local ip your PC
 		},
 		callbacks: {
 			ready: function (err, browserSync) {
