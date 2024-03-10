253ced2527af8c851914afb87be372215054507f
Nezhdanov Sergei <Cenzor28@yandex.ru>
2024-03-10 19:48:06 +0400

    hide preview

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
+const video = document.querySelector('.video')
 const imgVideo = document.querySelector('.video__img')
 const imgIframe = document.querySelector('.video__iframe')
+
+let visivble = false
 
 
 const swiper = new Swiper('.swiper', {
@@ -17,3 +20,14 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
+
+video.addEventListener('click', () =>{
+  if (visivble) {
+    imgVideo.style = 'display: block;'
+    visivble = false
+  }
+  else {
+    imgVideo.style = 'display: none;'
+    visivble = true
+  }  
+})
